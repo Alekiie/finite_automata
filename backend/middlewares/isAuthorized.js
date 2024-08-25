d9f129a09f5b7ae2267eb7e1400db8cd193abbfa
alekiie <amnjogu78@gmail.com>
2024-08-25 18:17:33 +0300

    refactor: Remove isAuthorized middleware for instructors
    The `isAuthorized.js` file in the `middlewares` directory was deleted, removing the `authorizeInstructor` middleware function. This change removes the restriction on certain actions to only instructors.

--- backend/middlewares/isAuthorized.js
+++
@@ -1,8 +0,0 @@
-const authorizeInstructor = (req, res, next) => {
-    if (req.user.role !== 'instructor') {
-      return res.status(403).json({ message: "Forbidden: Only instructors can perform this action" });
-    }
-    next();
-  };
-
-  module.exports = authorizeInstructor;
