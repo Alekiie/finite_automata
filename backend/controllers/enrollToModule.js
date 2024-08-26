5ec86520e4c4a3222075462a34913cc176fc7751
alekiie <amnjogu78@gmail.com>
2024-08-26 13:21:53 +0300

    refactor: Update enrollToModule controller to include user authentication
    This commit updates the `enrollToModule.js` controller to include user authentication. It checks if the user is authenticated by verifying the token provided in the Authorization header. Additionally, it retrieves the user associated with the decoded token and uses their ID for enrollment validation. This change ensures that only authenticated users can enroll in modules, improving the security of the application.
    
    Note: This message is based on the provided code changes and recent commits.

--- backend/controllers/enrollToModule.js
+++ backend/controllers/enrollToModule.js
@@ -1,20 +1,32 @@
-const Enrollment = require('../models/EnrolledModules'); // Updated the model import to 'Enrollment'
+const Enrollment = require('../models/EnrolledModules'); 
+const Users = require('../models/UserModel');
+const jwt = require("jsonwebtoken");
+
 
 const enrollToModule = async (req, res) => {
   try {
-    if (!req.user) {
-      return res.status(401).json({ message: 'Unauthorized' });
+    const authHeader = req.headers.authorization;
+    // Check if the Authorization header is present
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({ message: "Unauthorized: No token provided" });
     }
 
+    const token = authHeader.split(" ")[1];
+
+    // Decode and verify the token
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+
+    // Find the user associated with the decoded token
+    const user = await Users.findById(decoded.id);
     const { moduleId } = req.body;
 
-    const existingEnrollment = await Enrollment.findOne({ userId: req.user._id, moduleId });
+    const existingEnrollment = await Enrollment.findOne({ userId: user._id, moduleId });
     if (existingEnrollment) {
       return res.status(400).json({ message: 'You are already enrolled in this module.' });
     }
 
     const enrollment = new Enrollment({
-      userId: req.user._id,
+      userId:user._id,
       moduleId
     });
 
