b6356b88bc4a1eeff34409f75813e18db7f174d4
Washington-Kimani <kimaniwashington39@gmail.com>
2024-09-04 10:19:40 +0300

    Some Changes

--- frontend/src/components/ModuleContent.jsx
+++ frontend/src/components/ModuleContent.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useContext } from "react";
 import { useParams, useNavigate, useLocation } from "react-router-dom";
+import { Document, Page } from "react-pdf";
 import axios from "../configs/axios";
 import AuthContext from "../context/AuthContext";
 
@@ -11,6 +12,7 @@ const ModuleContent = () => {
   const [module, setModule] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [numPages, setNumPages] = useState(null);
 
   const { modules, currentIndex } = location.state || {
     modules: [],
@@ -45,30 +47,51 @@ const ModuleContent = () => {
     }
   };
 
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  };
+
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error}</div>;
 
   return (
-    <div className="container relative mx-auto p-6">
-      <div className="w-5/6 flex absolute -top-52 left-0 justify-between mt-6">
+    <div className="container mx-auto p-6">
+      <h1 className="text-3xl font-semibold mb-6">{module.title}</h1>
+      <p className="text-lg mb-4">{module.description}</p>
+
+      {module.content.type === "pdf" && (
+        <div className="pdf-viewer">
+          <Document
+            file={`/path/to/your/public/directory/${module.content.url}`}
+            onLoadSuccess={onDocumentLoadSuccess}
+          >
+            {Array.from(new Array(numPages), (el, index) => (
+              <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+            ))}
+          </Document>
+        </div>
+      )}
+
+      {module.content.type !== "pdf" && (
+        <div className="content">{module.content.title}</div>
+      )}
+
+      <div className="flex justify-between mt-6">
         <button
-          className="bg-blue-500 text-gray-800 px-4 py-2 rounded disabled:opacity-50"
+          className="bg-gray-300 text-gray-800 px-4 py-2 rounded disabled:opacity-50"
           onClick={() => handleNavigation(currentIndex - 1)}
           disabled={currentIndex === 0}
         >
           Previous
         </button>
         <button
-          className="bg-blue-500 text-gray-800 px-4 py-2 rounded disabled:opacity-50"
+          className="bg-gray-300 text-gray-800 px-4 py-2 rounded disabled:opacity-50"
           onClick={() => handleNavigation(currentIndex + 1)}
           disabled={currentIndex === modules.length - 1}
         >
           Next
         </button>
       </div>
-      <h1 className="text-3xl font-semibold mb-6">{module.title}</h1>
-      <p className="text-lg mb-4">{module.description}</p>
-      <div className="content">{module.content.title}</div>
     </div>
   );
 };
