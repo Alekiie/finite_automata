2611bdf885f0486b6fc88d44d455ace3c62c24f2
alekiie <amnjogu78@gmail.com>
2024-09-04 10:37:20 +0300

    chore: Update enrolled.controllers.js to populate module details in allEnrollments

--- backend/controllers/enrolled.controllers.js
+++ backend/controllers/enrolled.controllers.js
@@ -6,7 +6,7 @@ const authenticateUser = require("../utils/auth.util");
 const enrolled = async (req, res) => {
   try {
     const { user, status, json } = await authenticateUser(req, res);
-    const allEnrollments = await Enrollment.findOne({ userId: user._id });
+    const allEnrollments = await Enrollment.find({ userId: user._id }).populate('moduleId',"_id title description author automataReference enrolledUsers createdAt updatedAt");
     // Return all enrolled modules
     return res.status(200).json(allEnrollments);
   } catch (error) {
